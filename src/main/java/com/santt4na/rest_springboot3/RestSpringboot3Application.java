9e3e2a76776a413ad733248657172f3f2d926cfa
Santt4naDev <jhonnsantt4na@gmail.com>
2025-02-26 19:46:36 -0300

    first commit

---
+++ src/main/java/com/santt4na/rest_springboot3/RestSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.santt4na.rest_springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/santt4na/rest_springboot3/RestSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4na.rest_springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
