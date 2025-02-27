07b9088239b24241e533d06e2bbe6fed00178694
Santt4naDev <jhonnsantt4na@gmail.com>
2025-02-27 19:50:28 -0300

    PathVariable X RequestParam

---
+++ src/main/java/com/santt4na/rest_springboot3/controller/MathController.java
@@ -0,0 +1,40 @@
+package com.santt4na.rest_springboot3.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class MathController {
+   // private static final String template = "Helo, %s";
+   // private final AtomicLong counter = new AtomicLong();
+
+   /*
+    * RequestMapping -> Basicamente é uma anotação generica
+    * Podendo ser mais especifico com o GetMapping, PostMapping, DeleteMapping
+    * etc..
+    * 
+    * Diferenças entre o @PathVariable X @RequestParam
+    * Ambos são usados para extrair informações da URL, mas de maneiras diferentes:
+    * 
+    * @PathVariable -> Obrigatorio passar
+    * Captura valores que fazem parte do caminho da URL.
+    * ex: http://localhost:8080/usuarios/123
+    * 123 é capturado.
+    * 
+    * @RequestParam -> Opcional ser passado, Captura valores de parâmetros de
+    * consulta (query parameters) da URL.
+    * ex: http://localhost:8080/usuarios?nome=Joao
+    * Joao é capturado como o valor do parâmetro nome.
+    */
+   @RequestMapping(value = "/sum/{numOne}/{numTwo}", method = RequestMethod.GET)
+   public String sum(
+         @PathVariable(value = "numOne") String numOne,
+         @PathVariable(value = "numTwo") String numTwo) {
+      return numOne + " " + numTwo;
+   }
+
+}
